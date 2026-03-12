a486ff47885cce12bc2c1775d1e292d43d9376a1
MarcHunter87 <marchunter888@gmail.com>
2026-03-12 17:06:41 +0100

    Refactor import path for authInterceptor to improve project structure and maintainability.

---
+++ dishly-frontend/src/app/Core/interceptors/auth-interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthServices } from '../../Pages/auth/Services/auth-services';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthServices);
+  const token = authService.getToken();
+
+  if (token) {
+    if (typeof ngDevMode === 'undefined' || ngDevMode) {
+      console.log('JWT present in request:', req.method, req.url);
+    }
+    const authReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
+    });
+    return next(authReq);
+  }
+
+  if (typeof ngDevMode === 'undefined' || ngDevMode) {
+    console.log('No token, request without Authorization header:', req.method, req.url);
+  }
+  return next(req);
+};
