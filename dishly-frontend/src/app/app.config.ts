5efa04f847245ee576787bc59992ee75720b288f
MarcHunter87 <marchunter888@gmail.com>
2026-03-12 17:46:03 +0100

    Config Changes

--- dishly-frontend/src/app/app.config.ts
+++ dishly-frontend/src/app/app.config.ts
@@ -10,7 +10,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { authInterceptor } from './Core/interceptors/auth-interceptor';
 
 import {
   ChefHat,
